cbb263e42e38353750cb4382766551d8582f95df
priamixdj <132686877+priamixdj@users.noreply.github.com>
2025-04-25 09:43:55 +0200

    update

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -4,8 +4,8 @@ let text = document.getElementById('menu_text');
 
 let menu = document.getElementById('menu');
 
-// seconda opzione  -> testo
-hamburger_menu.addEventListener('click', function() {
+function closeMenu() {
+    console.log('closeMenu');
     hamburger_menu.classList.toggle('menu_text_open');
 
     if (hamburger_menu.classList.contains('menu_text_open')) {
@@ -22,8 +22,12 @@ hamburger_menu.addEventListener('click', function() {
 
     menu.classList.toggle('menu_open');
     menu.classList.toggle('menu_close');
-        
+}
 
+
+// seconda opzione  -> testo
+hamburger_menu.addEventListener('click', function() {
+    closeMenu();
 });
 
 
@@ -31,14 +35,18 @@ hamburger_menu.addEventListener('click', function() {
 // quando clicchi un opzione del menu, chiudi il menu
 let menu_items = [
     document.getElementById('home'),
+    document.getElementById('playlists'),
     document.getElementById('artists'),
     document.getElementById('about'),
+    document.getElementById('team'),
     document.getElementById('contact')
 ];
 
 for(let i = 0; i < menu_items.length; i++) {
     menu_items[i].addEventListener('click', function() {
-        menu.classList.toggle('menu_close');
+        closeMenu();
     });
 }
+
+
 
