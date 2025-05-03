2721b5a46117f6abb706c0b33bfec3f6cbb2b1a9
priamixdj <132686877+priamixdj@users.noreply.github.com>
2025-05-03 20:31:23 +0200

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,10 @@
-// // Create a black circle element
-// const circle = document.createElement('div');
-// circle.style.position = 'absolute';
-// circle.style.width = '20px';
-// circle.style.height = '20px';
-// circle.style.backgroundColor = 'black'; // Cambiato il colore in nero
-// circle.style.borderRadius = '50%';
-// circle.style.pointerEvents = 'none'; // Ensure it doesn't block mouse events
-// circle.style.zIndex = '-1'; // Ensure it stays behind the cursor
-// document.body.appendChild(circle);
+// Create a black circle element
+const circle = document.createElement('div');
+    circle.classList.add('CircleMouse');
 
-// // Update circle position on mouse move
-// document.addEventListener('mousemove', (event) => {
-//     circle.style.left = `${event.pageX - 10}px`; // Center the circle on the cursor
-//     circle.style.top = `${event.pageY - 10}px`;
-// });
+document.body.appendChild(circle);
+
+document.addEventListener('mousemove', (event) => {
+    circle.style.left = `${event.pageX - 10}px`; // Center the circle on the cursor
+    circle.style.top = `${event.pageY - 10}px`;
+})
