753c2c45e17189cedfcc1c3a58eb363710323bb8
Mattepriagggg <132686877+priamixdj@users.noreply.github.com>
2025-05-26 07:05:24 +0200

    coming soon page

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,3 +1,31 @@
+// aggiunta dl menu
+function addMenu() {
+    return `
+        <section id="menu" class="menu_close">
+            <div class="menu_content">
+            <a id="home" href="index.html"><h1>Home</h1></a>
+            <a id="artists" href="#top_artists"><h1>Top artists</h1></a>
+            <a id="playlists" href="#playlist"><h1>Playlists</h1></a>
+            <a id="about" href="about.html"><h1>About</h1></a>
+            <a id="team" href="team.html"><h1>Team</h1></a>
+            <a id="contact" href="#footer"><h1>Contacts</h1></a>
+            </div>
+        </section>
+        
+
+        <section id="home">
+            <!-- <img src="assets/image/logoWhite.svg" alt="Logo UrbanSite" class="logo"> -->
+
+            <!-- tre span per il menu -->
+            <h1 id="menu_text" class="">menu.</h1>
+            <span id="hamburger_menu"></span>
+        </section>
+
+        <header class="section-black" id="prima_section">
+            <img src="assets/image/Logo png/3000 x 3000/2.23white.png" alt="Urban Logo">
+        </header>
+    `;
+}
 
 let hamburger_menu = document.getElementById('hamburger_menu');
 let text = document.getElementById('menu_text');
