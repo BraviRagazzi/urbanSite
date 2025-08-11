6c468fd733f23bdd39068ddf1156c791eb5b44f1
priamixdj <132686877+priamixdj@users.noreply.github.com>
2025-08-11 10:19:44 +0200

    pp

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,53 +5,10 @@ const circle = document.createElement('div');
 
 // document.body.appendChild(circle);
 
-
-
-
-document.addEventListener('mousemove', (event) => {
-    circle.style.left = `${event.pageX - 10}px`; // Center the circle on the cursor
-    circle.style.top = `${event.pageY - 10}px`;
-})
-
-
-// creare la privacy policy 
-function openPrivacyPolicy() {
-
-    if (document.getElementById('privacypolicy')) {
-        console.log('Privacy Policy already exists');
-        return; // Exit if the privacy policy is already open
-    }
-
-    const privacyPolicy = document.createElement('div');
-
-    privacyPolicy.id = 'privacypolicy';
-
-    privacyPolicy.classList.add('privacy-policy');
-    privacyPolicy.innerHTML = `
-        <h2>Privacy Policy</h2> <br>
-        <h4>- Dati inviati dall' utente </h4>
-        I Dati inviati dall' utente non verranno in nessun modo trattati o divulgati, ma verranno soltanto inviati alla mail info@urban.it per informazioni o richieste dell' utente stesso.
-        <br><br>Questo garantisce una maggiore privacy per gli utenti e una maggiore riservatezza dei dati
-        
-        <br> <br>
-        </p>
-        <button id="closePrivacyPolicy">Close</button>
-    `;
-    document.body.appendChild(privacyPolicy);
-
-    document.getElementById('closePrivacyPolicy').addEventListener('click', () => {
-        document.body.removeChild(privacyPolicy);
-    });
-
-    console.log('Privacy Policy opened');
-}
-
-
-// loading
-// let loading = document.getElementById('loading');
-// window.addEventListener('load' , () => {
-//     loading.style.display = 'block';
+// cerchio del mouse
+// document.addEventListener('mousemove', (event) => {
+//     circle.style.left = `${event.pageX - 10}px`; // center the circle on the cursor
+//     circle.style.top = `${event.pageY - 10}px`;
 // })
 
-// loading.style.display = 'none';
 
